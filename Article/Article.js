5469b77aa095c233c79adcb926c6c83e9d2f82f5
tauan-longaretti <tauanlongaretti@gmail.com>
2019-11-05 23:33:36 -0500

    created a function that creates a component

--- Article/Article.js
+++ Article/Article.js
@@ -2,8 +2,8 @@
 /* Look over this data, then proceed to line 91*/
 const data = [
   {
-    title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    cardTitle: 'Lambda School Students: "We\'re the best!"',
+    cardDate: 'Nov 5th, 2018',
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    cardTitle: 'Javascript and You, ES6',
+    cardDate: 'May 7th, 2019',
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    cardTitle: 'React vs Angular vs Vue',
+    cardDate: 'June 7th, 2019',
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    cardTitle: 'Professional Software Development in 2019',
+    cardDate: 'Jan 1st, 2019',
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -91,8 +91,8 @@ const data = [
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
   
   <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
+    <h2>{cardTitle of the article}</h2>
+    <p class="cardDate">{cardDate of the article}</p>
 
     {three separate paragraph elements}
 
@@ -112,3 +112,30 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+const createCard = (title, date, firstParagraph, secondParagraph, thirdParagraph) => {
+const card = document.createElement('div');
+const cardTitle = document.createElement('h2');
+const cardDate = document.createElement('p');
+const paragraphOne = document.createElement('p');
+const paragraphTwo = document.createElement('p');
+const paragraphThree = document.createElement('p');
+const expandButton = document.createElement('span');
+
+card.appendChild(cardTitle);
+card.appendChild(cardDate);
+card.appendChild(paragraphOne);
+card.appendChild(paragraphTwo);
+card.appendChild(paragraphThree);
+card.appendChild(expandButton);
+
+card.classList.add('article');
+cardDate.classList.add('cardDate');
+expandButton.classList.add('expandButton')
+
+return card;
+}
+
+const parentComponent = document.querySelector('.articles');
+const cardComponent = createCard('faketitle', 'fakedate', 'fakefirstparagraph', 'fakesecondparagraph', 'fakethirdparagraph');
+parentComponent.appendChild(cardComponent);
+
